27f66137145307ab23037199580df116239b0ab8
gujaraidze <gujagujara@gmail.com>
2026-04-25 19:14:13 +0400

    filter card cleanup

--- app/src/main/java/com/example/ecommerce_app/components/FilterCard.kt
+++ app/src/main/java/com/example/ecommerce_app/components/FilterCard.kt
@@ -11,10 +11,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.ecommerce_app.model.Data
 import com.example.ecommerce_app.model.Filter
 
 @Composable
@@ -41,13 +39,4 @@ fun FilterCard(
             fontWeight = FontWeight(600)
         )
     }
-}
-
-@Composable
-@Preview(showBackground = true)
-fun FiltercardPreview() {
-    FilterCard(
-        filterItem = Data.filters[0],
-        onClick = {}
-    )
 }
