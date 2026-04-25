cba6f8a31c4a672e141dd2469588beeb776215d8
gujaraidze <gujagujara@gmail.com>
2026-04-25 16:47:44 +0400

    liked product screen fix

--- app/src/main/java/com/example/ecommerce_app/components/LikedProductGrid.kt
+++ app/src/main/java/com/example/ecommerce_app/components/LikedProductGrid.kt
@@ -16,20 +16,20 @@ fun LikedProductGrid(
     onFavoriteClick: (ClothingItem) -> Unit,
 
 ) {
+    val favoriteClothes = clothes.filter { it.isFavorite }
+
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        items(clothes) { cloth ->
-            if (cloth.isFavorite) {
-                LikedProductCard(
-                    clothingItem = cloth,
-                    onItemClick = {onClick.invoke(cloth)},
-                    onFavoriteClick = { onFavoriteClick(cloth) }
-                )
-            }
+        items(favoriteClothes) { cloth ->
+            LikedProductCard(
+                clothingItem = cloth,
+                onItemClick = { onClick.invoke(cloth) },
+                onFavoriteClick = { onFavoriteClick(cloth) }
+            )
         }
     }
 }
